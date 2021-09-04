5a33bbaaa2c1be346e99f54408e4294b4798d588
RobertoECC <88851027+RobertoECC@users.noreply.github.com>
2021-09-03 20:32:19 -0500

    Update exercise.py

--- assignments/17NCuadradoMayor/src/exercise.py
+++ assignments/17NCuadradoMayor/src/exercise.py
@@ -1,8 +1,17 @@
 
 
 def main():
+    num = int(input("Escribe un numero: "))
     #Escribe tu código debajo de esta línea
     pass
+a = 1 
+
+while a < num:
+    if (a ** 2) < num:
+        a = a + 1 
+    elif (a ** 2) > num:
+        print(a)
+        break
 
 if __name__=='__main__':
     main()
